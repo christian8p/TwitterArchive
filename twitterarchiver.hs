5d566ec1369398e88ca56fc0be2494e30eb18e19
Deepak Jois <deepak.jois@gmail.com>
2009-08-14 21:41:56 -0700

    Making JSON the default output format

--- twitterarchiver.hs
+++ twitterarchiver.hs
@@ -5,6 +5,7 @@ import Network.URI
 import Text.JSON
 import Text.JSON.Types
 import Text.JSON.String
+import Text.JSON.Pretty
 
 import Ratio
 
@@ -23,10 +24,16 @@ import qualified System.IO.UTF8 as UTF8
 -- data structure for tweet
 data Tweet = Tweet { tweetText :: String, 
                      tweetCreatedAt :: String , 
-                     tweetId :: String }
+                     tweetId :: String } deriving Show
 
--- data structure for twitter stream
-data TwitterStream = TwitterStream String [Tweet]
+instance JSON Tweet where
+    showJSON (Tweet tweetText tweetCreatedAt tweetId) = makeObj [
+                                                                  ("text", JSString (JSONString tweetText)),
+                                                                  ("created_at", JSString (JSONString  tweetCreatedAt)), 
+                                                                  ("id", JSRational False 
+                                                                                    (fromInteger (read tweetId :: Integer)))
+                                                                ]
+    readJSON tweet = Error "not yet implemented"
 
 twitterUrl  = "http://twitter.com/"
 
@@ -58,15 +65,13 @@ main = do
 
          -- Here we thread startOptions through all supplied option actions
          opts <- foldl (>>=) (return defaultOptions) actions
-         
          let Options { optUsername = username } = opts
+
          tweetsJSON <- readTwitterStream username
          
          let tweets        = map extractTweet tweetsJSON                                   
-             twitterStream = TwitterStream username tweets 
-             tweetsString  =  formatTweets twitterStream
-         
-         UTF8.writeFile "archive.txt"  (unlines $ intersperse "\n" tweetsString)
+             tweetsString  =  render $  pp_value  $ showJSON tweets
+         UTF8.writeFile "archive.txt"  tweetsString
 
 extractTweet :: JSValue -> Tweet
 extractTweet tweetJSON = Tweet { tweetText = t, tweetCreatedAt = c, tweetId = i  }
@@ -77,18 +82,6 @@ extractTweet tweetJSON = Tweet { tweetText = t, tweetCreatedAt = c, tweetId = i 
                                     Just (JSString (JSONString s)) -> s
                                     Just (JSRational False a) -> show (numerator a)
                            [t,c,i]  = map ex ["text", "created_at", "id"]
-
-formatTweets :: TwitterStream -> [String]
-formatTweets twitterstream = map formatTweet tweets
-                             where
-                               TwitterStream username tweets = twitterstream
-                               formatUrl statusId =  concat [twitterUrl,
-                                                             username,
-                                                             "/status/", 
-                                                             statusId]
-                               formatTweet tweet = unlines [ (tweetText tweet), 
-                                                             (tweetCreatedAt tweet),
-                                                             formatUrl (tweetId tweet)]
 
 readTwitterStream username = readTwitterStream' username 1 []
 
