73d89f3b347fdbf89c95bba11ae9970b6ea8bd9b
Deepak Jois <deepak.jois@gmail.com>
2012-01-19 02:07:24 -0800

    Updating to hs-twitterarchiver 0.2

--- archive.rb
+++ archive.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-%w( vyom nimbupani divya ).each { |id| system "hs-twitterarchiver -u #{id} -f #{id}.json" }
+%w( vyom nimbupani divya ).each { |id| system "hs-twitterarchiver #{id}" }
